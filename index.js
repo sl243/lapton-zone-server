72b60164e6468503e95d2d79986346ce40d25033
sl243 <slmiucse@gmail.com>
2022-11-26 02:47:09 +0600

    added product order post method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express()
@@ -15,11 +15,51 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2elctou.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try{
+    const categoriesCollection = client.db('laptobZone').collection('categories')
+    const productsCollection = client.db('laptobZone').collection('products')
+    const buyCollection = client.db('laptobZone').collection('buy')
+
+    app.get('/categories', async(req, res) => {
+      const query = {}
+      const categories = await categoriesCollection.find(query).toArray()
+      res.send(categories)
+    })
+
+
+    // // categories:id 
+    // app.get('/categories/:id', async(req, res) => {
+    //   const id = req.params.id;
+    //   const query = {_id: ObjectId(id)}
+    //   const products = await categoriesCollection.find(query).toArray();
+    //   res.send(products)
+    // })
+
+    // categories:id 
+    app.get('/categories/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {category_id: id}
+      const products = await categoriesCollection.find(query).toArray();
+      res.send(products)
+    })
+
+    // user product buy
+    app.post('/buy', async(req, res) => {
+      const buy = req.body;
+      const result = await buyCollection.insertOne(buy);
+      res.send(result)
+    })
+
+
+  }
+
+  finally{
+
+  }
+}
+run().catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
